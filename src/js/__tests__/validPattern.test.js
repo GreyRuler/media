94b45e423aaedf217df3618b1c7c1c4f17d0670c
GreyRuler <daniil_vasilenko757@mail.ru>
2023-04-08 12:44:20 +0300

    fix pattern

--- src/js/__tests__/validPattern.test.js
+++ src/js/__tests__/validPattern.test.js
@@ -5,7 +5,7 @@ test.each([
 	{ actual: '51.50851,-0.12572', expected: false },
 	{ actual: '[51.50851, -0.12572]', expected: false },
 ])('Должен проверить строки на валидность', ({ actual, expected }) => {
-	const pattern = /-?\d+.\d+, [—-]?\d+.\d+/;
+	const pattern = /(?<!.)-?\d+.\d+, [—-]?\d+.\d+(?!.)/;
 	const re = new RegExp(pattern);
 	expect(re.test(actual)).toBe(expected);
 });
